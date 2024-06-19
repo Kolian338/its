502201f8a24519f2060f77562eeb531e1d67f8f6
Nikolay Safonov <kolian338@gmail.com>
2024-06-19 12:54:41 +0200

    - Вынес констаны в енумы - Добавил описание параметров

--- settings.py
+++ settings.py
@@ -1,12 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from enum import Enum
+from pydantic import Field
+
+
+class MsourceEnum(str, Enum):
+    BACKMEGAPOLISURL2 = 'backMegapolisUrl2'
+    BACKMEGAPOLISURL = 'backMegapolisUrl'
 
 
 class Settings(BaseSettings):
     """
-    Класс для настроек проектов.
-
-    itec_url: url для сервиса itec
-    tla_url: url для сервиса lta
+    Класс для настроек проектов. По приоритету берется из .env.
     По умолчанию заданы дефолтные url на случай если в .env их нет.
     """
 
@@ -16,9 +20,22 @@ class Settings(BaseSettings):
         env_nested_delimiter='.'
     )
 
-    itec_url: str = 'http://192.168.32.67:3000'
-    tla_url: str = 'http://traffic-lights-api-develop.k8.sccloud.ru'
-    msource: str = 'backMegapolisUrl2'
+    itec_url: str = Field(
+        'http://192.168.32.67:3000',
+        description='url для сервиса itec'
+    )
+    tla_url: str = Field(
+        'http://traffic-lights-api-develop.k8.sccloud.ru',
+        description='url для сервиса tla'
+    )
+    msource: str = Field(
+        MsourceEnum.BACKMEGAPOLISURL2,
+        description=(
+            'адрес для запросов от TLA в Мегаполис из конфига:'
+            'https://gitlab.sccloud.ru/dis'
+            '/traffic-lights-api/-/blob/develop/config.toml'
+        )
+    )
 
 
 base_settings = Settings()
