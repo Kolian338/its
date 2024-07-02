17f6a644f35cad658b273f3c5042787a6a2a0a02
Nikolay Safonov <kolian338@gmail.com>
2024-07-02 15:05:10 +0200

    Файл конфигурации для pytest

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
         env_file='.env',
         env_file_encoding='utf-8',
         env_nested_delimiter='.',
-        extra='allow'
+        extra='ignore'
     )
     path: str
 
